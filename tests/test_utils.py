865687cd7ba0ccecf16f0a6bb727cc84fd3b4dfe
Marc Alvidrez <cram@greenavenger.com>
2025-02-07 09:30:31 -0800

    Hex digits can be uppercase, too

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,6 +13,13 @@ def test_extract_uuid_plain():
     assert result == expected
 
 
+def test_extract_uuid_uppercase_plain():
+    text = "A73EC36F655C9452CF88F50E99CBA946"
+    result = extract_uuid(text)
+    expected = uuid.UUID("a73ec36f655c9452cf88f50e99cba946")
+    assert result == expected
+
+
 def test_extract_uuid_dashed():
     text = "a73ec36f-655c-9452-cf88-f50e99cba946"
     result = extract_uuid(text)
@@ -20,8 +27,22 @@ def test_extract_uuid_dashed():
     assert result == expected
 
 
+def test_extract_uuid_uppercase_dashed():
+    text = "A73EC36F-655C-9452-CF88-F50E99CBA946"
+    result = extract_uuid(text)
+    expected = uuid.UUID("a73ec36f-655c-9452-cf88-f50e99cba946")
+    assert result == expected
+
+
 def test_extract_uuid_with_noise():
     text = 'itemid="/#a73ec36f655c9452cf88f50e99cba946-episodes">'
+    result = extract_uuid(text)
+    expected = uuid.UUID("a73ec36f655c9452cf88f50e99cba946")
+    assert result == expected
+
+
+def test_extract_uuid_uppercase_with_noise():
+    text = 'itemid="/#A73EC36F655C9452CF88F50E99CBA946-episodes">'
     result = extract_uuid(text)
     expected = uuid.UUID("a73ec36f655c9452cf88f50e99cba946")
     assert result == expected
